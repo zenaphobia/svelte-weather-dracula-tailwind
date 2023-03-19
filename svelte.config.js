bc7946344f9e93298cb49d21c3ca1b6a161b80b1
zenaphobia <100097619+zenaphobia@users.noreply.github.com>
2023-03-18 22:06:13 -0700

    trying netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
-
+import adapter from '@sveltejs/adapter-netlify';
+ 
 export default {
   kit: {
+    // default options are shown
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+ 
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
     })
   }
 };
