fd51b0f960d0f4aeb846315a8fd69370b9136b74
zenaphobia <100097619+zenaphobia@users.noreply.github.com>
2023-03-18 21:28:11 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+    import logo from '../icons8-cloud-lightning.gif'
+</script>
+
+<div class="container-full drac-bg-purple">
+  <div class="max-w-7xl mx-auto">
+    <div class="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+      <div class="text-slate-800 text-2xl logo font-bold">Quick Weather</div>
+    </div>
+  </div>
+</div>
+
+  <slot />
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: []
+};
