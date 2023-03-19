bec6c19d182d2e9d9378c8869a35fea17af6978b
zenaphobia <100097619+zenaphobia@users.noreply.github.com>
2023-03-18 22:00:31 -0700

    edits

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,13 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
   }
 };
-
